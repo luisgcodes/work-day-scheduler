0a528029c430be866267ac1ae46d837bd250ab67
luisgcodes <lcienfuegos95@gmail.com>
2022-03-24 11:24:41 -0700

    added date/time

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Current Day/Time
+var presentDay = $("#currentDay").text(moment().format("dddd, MMMM Do"));
+var presentTime = $("#currentTime").text(moment().format("HH:mm A"));
+
+presentTime = function(){
+    
+}
