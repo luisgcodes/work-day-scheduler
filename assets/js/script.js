57eb53bd9367c5752103e3ccacc2657b6f3d5395
luisgcodes <lcienfuegos95@gmail.com>
2022-03-24 16:15:30 -0700

    added color function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
-// Current Day/Time
+// // Current Day/Time
+var schedule = {};
 var presentDay = $("#currentDay").text(moment().format("dddd, MMMM Do"));
-var presentTime = $("#currentTime").text(moment().format("HH:mm A"));
+var presentTime = $("#currentTime").text(moment().format("hh:mm A"));
 
-presentTime = function(){
-    
-}
+
+var presentTime = moment().hour();
+
+var hourColor = function() {
+    $(".description").each(function () {
+        var hourN = parseInt($(this).attr("id"));
+        if (hourN < presentTime) {
+            $(this).addClass("past");
+        } else if (hourN === presentTime) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            
+        } else {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            
+        }
+    });
+};
+
+// var event = function
+
+hourColor();
