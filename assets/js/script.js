c866f8a83bd5cc1eadd2f980dbf4388259aa2ac8
luisgcodes <lcienfuegos95@gmail.com>
2022-03-24 23:10:02 -0700

    fixed localStorage bugs/added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,50 @@
-// // Current Day/Time
-var schedule = {};
-var presentDay = $("#currentDay").text(moment().format("dddd, MMMM Do"));
+var content = {};
+// current date/time
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 var presentTime = $("#currentTime").text(moment().format("hh:mm A"));
 
-
-var presentTime = moment().hour();
-
-var hourColor = function() {
-    $(".description").each(function () {
-        var hourN = parseInt($(this).attr("id"));
-        if (hourN < presentTime) {
+var hourColor = function(){
+   
+    var presentTime = moment().hour();
+// distinguishes color for the time blocks
+    $(".time-block").each(function(){
+        var hourN = parseInt($(this).attr("id")); 
+        if(hourN < presentTime){
             $(this).addClass("past");
-        } else if (hourN === presentTime) {
-            $(this).addClass("present");
+        } else if (hourN===presentTime){
             $(this).removeClass("past");
-            
+            $(this).addClass("present");
         } else {
-            $(this).addClass("future");
             $(this).removeClass("past");
             $(this).removeClass("present");
-            
+            $(this).addClass("future");
         }
     });
 };
-
-// var event = function
-
+// save the description
+var saveContent = function(id,text){
+    content = JSON.parse(window.localStorage.getItem("content")) || {}
+    $.each(content,function(hour,description){
+        $('#' + hour).find("textarea").val(description);
+    })
+}
+var saveEvent = function() {
+    localStorage.setItem("content", JSON.stringify(content));
+  };
+var createEvent = function(id, text){
+    content[id] = text;
+    saveEvent();
+}
+// call functions
 hourColor();
+saveContent();
+
+// save btn function
+$(".saveBtn").click(function(){
+    var parentElement = $(this).parent();
+    var id = $(parentElement).attr("id"); 
+    var text = $(parentElement).find("textarea").val().trim(); 
+    createEvent(id,text);
+});
+
+
